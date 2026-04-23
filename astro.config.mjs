28b01db22de185b585538009e14fab406ca53dac
Edwin Rivera <gaedwin92@gmail.com>
2026-04-23 12:02:31 -0600

    Correccion a la hora de subir

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,11 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://paneldetecho.com',
+  site: 'https://landing.mequipo.com',
+  base: '/landings/paneldetecho', // <--- Agrega esto
+  trailingSlash: 'always',
+  build: {
+    format: 'directory',
+  },
   integrations: [sitemap()]
 });
